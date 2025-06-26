3f35c8daf3d858a7768f915a5cfcc2b6b7301729
Maxim Vishnevsky <mxmvshnvsk@gmail.com>
2025-06-26 10:49:32 +0300

    remove promo

--- src/router.js
+++ src/router.js
@@ -1,19 +1,14 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHistory } from 'vue-router'
 
 const routes = [
   {
     path: '/',
-    name: 'Welcome',
-    component: () => import('./modules/promo/PromoPage.vue'),
-  },
-  {
-    path: '/dashboard',
     name: 'Dashboard',
     component: () => import('./modules/dashboard/DashboardPage.vue'),
   },
-];
+]
 
 export const router = createRouter({
   history: createWebHistory(),
   routes,
-});
+})
